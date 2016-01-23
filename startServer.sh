7dbc54b49a73181c8eedc255044c70c828b8c1b7
Rytis Bizauskas <luxcapto@gmail.com>
2016-01-23 13:49:25 +0000

    Improved start server script

--- startServer.sh
+++ startServer.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-screen uwsgi -s /tmp/uwsgi.sock -w index:app  --chmod-socket=666
+screen -S flask-server -d -m uwsgi -s /tmp/uwsgi.sock -w index:app  --chmod-socket=666
+#screen uwsgi -s /tmp/uwsgi.sock -w index:app  --chmod-socket=666
