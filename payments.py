cff3c588b2f00b37009001de76c785bc66414fe7
Rytis Bizauskas <luxcapto@gmail.com>
2016-01-23 15:45:48 -0500

    Added a CapitalOne payments test script

---
+++ payments.py
@@ -0,0 +1,25 @@
+# This is written for PYTHON 3
+# Don't forget to install requests package
+
+import requests
+import json
+
+customerId = '56241a12de4bf40b17112139'
+apiKey = '33fa62096eeddb37f6c8b4f305d51a9a'
+
+url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId,apiKey)
+payload = {
+          "type": "Savings",
+            "nickname": "test",
+              "rewards": 10000,
+                "balance": 10000,
+                }
+# Create a Savings Account
+response = requests.post(
+            url,
+                data=json.dumps(payload),
+                    headers={'content-type':'application/json'},
+                        )
+
+if response.status_code == 201:
+        print('account created')
