c84204a3b4994f14f3bb45412ddb98e2014dba97
Rytis Bizauskas <luxcapto@gmail.com>
2016-01-24 03:22:35 +0000

    Added back nginx conf and improved server script

--- startServer.sh
+++ startServer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+screen -S flask-server -X quit
 screen -S flask-server -d -m uwsgi -s /tmp/uwsgi.sock -w index:app  --chmod-socket=666
 #screen uwsgi -s /tmp/uwsgi.sock -w index:app  --chmod-socket=666
--- stripe/stripe-php/config.php
+++ stripe/stripe-php/config.php
@@ -2,8 +2,8 @@
 require_once('vendor/autoload.php');
 
 $stripe = array(
-  "secret_key"      => "sk_live_",
-  "publishable_key" => "pk_live_"
+  "secret_key"      => "sk_test_NaA75FKH5oUZzpWYfovt0Cnr",
+  "publishable_key" => "pk_test_OBYVeh77IrfyvomwDXIX4Drq"
 );
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
